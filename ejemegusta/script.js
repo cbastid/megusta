3bde2494c1e74091131bb428055b3f3fe6c5d44f
Cristian Bastidas <cbastid.dr@gmail.com>
2022-10-18 15:55:20 -0300

    ejercicio me gusta

---
+++ ejemegusta/script.js
@@ -0,0 +1,6 @@
+let totlike = 0;
+function idchange(){
+        let idvar = document.querySelector("#like");
+        totlike++;
+        idvar.innerText = totlike + " like(s)"
+}
---
+++ ejemegusta2/script2.js
@@ -0,0 +1,18 @@
+let totlike = 0;
+function idchange(){
+        let idvar = document.querySelector("#like");
+        totlike++;
+        idvar.innerText = totlike + " like(s)"
+}
+let totlike2 = 0;
+function idchange2(){
+        let idvar2 = document.querySelector("#like2");
+        totlike2++;
+        idvar2.innerText = totlike2 + " like(s)"
+}
+let totlike3 = 0;
+function idchange3(){
+        let idvar3 = document.querySelector("#like3");
+        totlike3++;
+        idvar3.innerText = totlike3 + " like(s)"
+}
